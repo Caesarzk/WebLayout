a50c0f61319cefd897025479556c5c07ce1d2618
Артём Михалькевич <mihalkevichartiom@gmail.com>
2022-12-21 18:33:57 +0300

    test

--- sendmail.php
+++ sendmail.php
@@ -1,32 +1,60 @@
 <?php
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+
+    $name = $_POST["name"];
+    $tel = $_POST["tel"];
 
     require 'phpmailer/src/Exception.php';
+    require 'phpmailer/src/SMTP.php';
     require 'phpmailer/src/PHPMailer.php';
 
+    use PHPMailer/PHPMailer/PHPMailer;
+    use PHPMailer/PHPMailer/SMTP;
+    use PHPMailer/PHPMailer/Exception;
+
     $mail = new PHPMailer(true);
-    $mail->CharSet = 'UTF-8';
-    $mail->setLanguage('ru', 'phpmailer/language/');
-    $mail->IsHTML(true);
 
-    $mail->setFrom('mihalkevichartiom@gmail.com', 'Johnny Paso');
-    $mail->addAddress('mihalkevichartiom@gmail.com');
-    $mail->Subject = 'Запрос клиента Blanchard';
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
 
-    $body = '<h1>Запрос на телефонный звонок от:</h1>';
-    $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
-    $body.='<p><strong>Телефон:</strong> '.$_POST['tel'].'</p>';
+    $mail->Host = "smtp.example.com";
+    $mail->SMTPS = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
 
-    $mail->Body = $body;
+    $mail->Username = "you@example.com";
+    $mail->Password = "password";
 
-    if (!$mail->send()) {
-        $message = 'Что-то пошло не так.';
-    }
-    else {
-        $message = 'Ваш запрос отправлен.';
-    }
-    $response = ['message' => $message];
-    header('Content-type: application/json');
-    echo json_encode($response);
+    $mail->SetFrom($email, $name);
+    $mail->addAddress("mihalkevichartiom@gmail.com");
+
+    $mail->Subject = "Запрос клиента Blanchard";
+    $mail->Body = $name;
+
+    $mail->send();
+
+    // $title = 'Тема письма';
+
+
+    // $mail->CharSet = 'UTF-8';
+    // $mail->setLanguage('ru', 'phpmailer/language/');
+    // $mail->IsHTML(true);
+
+    // $mail->setFrom('mihalkevichartiom@gmail.com', 'Johnny Paso');
+    // $mail->addAddress('mihalkevichartiom@gmail.com');
+    // $mail->Subject = 'Запрос клиента Blanchard';
+
+    // $body = '<h1>Запрос на телефонный звонок от:</h1>';
+    // $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+    // $body.='<p><strong>Телефон:</strong> '.$_POST['tel'].'</p>';
+
+    // $mail->Body = $body;
+
+    // if (!$mail->send()) {
+    //     $message = 'Что-то пошло не так.';
+    // }
+    // else {
+    //     $message = 'Ваш запрос отправлен.';
+    // }
+    // $response = ['message' => $message];
+    // header('Content-type: application/json');
+    // echo json_encode($response);
 ?>
