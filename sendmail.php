faee2ea9089832787543a6da0d772829be435272
Артём Михалькевич <mihalkevichartiom@gmail.com>
2022-12-21 18:59:43 +0300

    test4

--- sendmail.php
+++ sendmail.php
@@ -30,31 +30,4 @@
     $mail->Body = $name;
 
     $mail->send();
-
-    // $title = 'Тема письма';
-
-
-    // $mail->CharSet = 'UTF-8';
-    // $mail->setLanguage('ru', 'phpmailer/language/');
-    // $mail->IsHTML(true);
-
-    // $mail->setFrom('mihalkevichartiom@gmail.com', 'Johnny Paso');
-    // $mail->addAddress('mihalkevichartiom@gmail.com');
-    // $mail->Subject = 'Запрос клиента Blanchard';
-
-    // $body = '<h1>Запрос на телефонный звонок от:</h1>';
-    // $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
-    // $body.='<p><strong>Телефон:</strong> '.$_POST['tel'].'</p>';
-
-    // $mail->Body = $body;
-
-    // if (!$mail->send()) {
-    //     $message = 'Что-то пошло не так.';
-    // }
-    // else {
-    //     $message = 'Ваш запрос отправлен.';
-    // }
-    // $response = ['message' => $message];
-    // header('Content-type: application/json');
-    // echo json_encode($response);
 ?>
